46f1acce1bbb0353bf6ae52cd3d9029bd2c23751
Ted Herambert <tedherambert2004@gmail.com>
2024-12-06 04:45:57 +0100

    bugfixes

--- src/app.js
+++ src/app.js
@@ -16,26 +16,27 @@ function updateLangBtns(language) {
 	const activeLangBtn = document.getElementById("active-lang-button");
 	const langDropdown = document.querySelector(".lang-dropdown");
   
+	if (langDropdown) {
+	  langDropdown.innerHTML = '';
+	  const languages = ['en', 'fr', 'es'].filter(lang => lang !== language);
+  
+	  languages.forEach(lang => {
+		const langListeItem = document.createElement("li");
+		langListeItem.classList.add("lang-dropdown-item");
+  
+		const langBtn = document.createElement("li");
+		langBtn.classList.add("lang-button");
+		langBtn.textContent = getLanguageName(lang);
+		langBtn.setAttribute("data-lang", lang);
+		langBtn.onclick = () => applyLanguage(lang);
+  
+		langListeItem.appendChild(langBtn);
+		langDropdown.appendChild(langListeItem);
+	  });
+	}
 
 	activeLangBtn.textContent = getLanguageName(language);
 	activeLangBtn.setAttribute("data-lang", language);
-	
-	langDropdown.innerHTML = '';
-	const languages = ['en', 'fr', 'es'].filter(lang => lang !== language);
-
-	languages.forEach(lang => {
-		const langListeItem = document.createElement("li");
-		langListeItem.classList.add("lang-dropdown-item");
-
-		const langBtn = document.createElement("li");
-		langBtn.classList.add("lang-button")
-		langBtn.textContent = getLanguageName(lang);
-		langBtn.setAttribute("data-lang", lang)
-		langBtn.onclick = () => applyLanguage(lang);
-		
-		langListeItem.appendChild(langBtn);
-		langDropdown.appendChild(langListeItem);
-	});
 }
 
 function getLanguageName(language) {
@@ -59,8 +60,12 @@ function loadLocalisation(language) {
 		localisation = data;
 		applyLanguage(language);
 	  })
-	  .catch(error => console.error('Error loading localisation:', error));
-  }
+	  .catch(error => {
+		console.error('Error loading localisation:', error);
+		applyLanguage('en');
+	  });
+}
+  
   
 
 document.addEventListener("DOMContentLoaded", () => {
